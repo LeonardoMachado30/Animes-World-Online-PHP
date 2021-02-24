e51ba10e220b084d2e8806ee0a02e1b03948028a
Flavio Leonardo Machado <60984765+LeonardoMachado30@users.noreply.github.com>
2021-02-24 18:57:17 -0300

    update

--- resources/views/animes/show.blade.php
+++ resources/views/animes/show.blade.php
@@ -5,15 +5,17 @@
 @section('content')
 
     <div class="container-sm">
-        <div class="box-show">
             <div class="row ">
-                <div class="col1">
+                <div class="col-5">
                     <h1>{{ $animes->title }}</h1>
                 </div>
             </div>
-                <video src="/video/{{ $animes->video }}" controls class="custom-video"></video>
+            <div class="row">
+                <div class="col">
+                    <video src="/video/{{ $animes->video }}" controls class="custom-video"></video>
+                </div>
+            </div>
         </div>
     </div>
-
 
 @endsection
